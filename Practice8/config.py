990c2642c113d03eb4ca655ff868f9f30f9143fd
Sabira <sabiratalgatova0902@gmail.com>
2026-03-27 01:24:51 +0500

    Add Practice8 - PhoneBook with functions and stored procedures

---
+++ Practice8/config.py
@@ -0,0 +1,8 @@
+def load_config():
+    return {
+        "host": "localhost",
+        "database": "phonebook_db",
+        "user": "postgres",
+        "password": "Nova2026!",
+        "port": 5432
+    }
